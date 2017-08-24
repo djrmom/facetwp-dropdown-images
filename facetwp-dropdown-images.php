3254691619f6b88b67f7038598b480e1cfc97807
Jenny Ragan <ruwebtech@gmail.com>
2017-08-24 08:55:17 -0400

    Checks for dropdown facets for tax/product_cat to only attach to those facets

--- facetwp-dropdown-images.php
+++ facetwp-dropdown-images.php
@@ -10,6 +10,8 @@ defined( 'ABSPATH' ) or exit;
 class FacetWP_Dropdown_Images {
 
 
+	public $facet_names = array();
+
 	function __construct() {
 
 		define( 'FWPDI_VERSION', '1.0.0' );
@@ -26,6 +28,8 @@ class FacetWP_Dropdown_Images {
 			return;
 		}
 
+		$this->find_facets();
+
 		// register assets
 		wp_register_script( 'fwpdi-front', FWPDI_URL . '/assets/js/msdropdown/jquery.dd.min.js', array( 'jquery' ), FWPDI_VERSION, true );
 		wp_register_style( 'fwpdi-front', FWPDI_URL . '/assets/css/msdropdown/dd.css', array(), FWPDI_VERSION );
@@ -37,6 +41,18 @@ class FacetWP_Dropdown_Images {
 		add_filter( 'facetwp_facet_html', array( $this, 'filter_html' ), 10, 2 );
 	}
 
+	/**
+	 * create an array of product category sources
+	 */
+	function find_facets() {
+	    $facets = FWP()->helper->get_facets();
+	    foreach ( $facets AS $facet ) {
+	        if ( isset( $facet['source'] ) && 'tax/product_cat' == $facet['source'] && isset( $facet['type'] )&& 'dropdown' == $facet['type'] ) {
+	            $this->facet_names[] = $facet['name'];
+            }
+        }
+    }
+
 	function render_assets() {
 		wp_enqueue_style( 'fwpdi-front' );
 		wp_enqueue_script( 'fwpdi-front' );
@@ -47,7 +63,12 @@ class FacetWP_Dropdown_Images {
      *
      * TODO: figure out a way to target only select box for a product_cat dropdown
 	 */
-	function add_inline() { ?>
+	function add_inline() {
+	    $facet_classes = array();
+		foreach ( $this->facet_names AS $facet ) {
+			$facet_classes[] = 'body .facetwp-facet-' . $facet . ' select';
+		}
+	    if ( !empty( $facet_classes ) ) : ?>
         <style>
             .dd img { width: 50px; display: inline-block; }
             .dd { width: 100% !important; }
@@ -55,15 +76,12 @@ class FacetWP_Dropdown_Images {
 		<script>
             (function($) {
                 $(document).on('facetwp-loaded', function() {
-                    try {
-                        $("body .facetwp-type-dropdown select").msDropDown();
-                    } catch(e) {
-                        alert(e.message);
-                    }
+                    $("<?php echo implode( ',', $facet_classes ); ?>").msDropDown();
                 });
             })(jQuery);
 		</script>
-	<?php }
+        <?php endif;
+	}
 
 	/**
      * filter html output to add data-image to select options
